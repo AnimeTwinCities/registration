e2432d66dd9c70ec9fd0cf1d360fcbeb755c126a
John J. Koniges <john@koniges.net>
2019-04-01 19:10:15 -0500

    Add history tables

---
+++ src/AppBundle/Entity/Organization/DepartmentHistory.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright (c) 2019. Anime Twin Cities, Inc.
+ *
+ * This project, including all of the files and their contents, is licensed under the terms of MIT License
+ *
+ * See the LICENSE file in the root of this project for details.
+ */
+declare(strict_types=1);
+
+
+namespace AppBundle\Entity\Organization;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use \AppBundle\Entity\User;
+
+/**
+ * Badge
+ *
+ * @ORM\Table(name="organization_department_history",
+ *     indexes={
+ *      @ORM\Index(columns={"created_by"}),
+ *      @ORM\Index(columns={"modified_by"})
+ *     }
+ * )
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\Organization\DepartmentHistoryRepository")
+ */
+class DepartmentHistory
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var Department
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Organization\Department")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="department_id", referencedColumnName="id")
+     * })
+     */
+    private $department;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="created_by", referencedColumnName="id")
+     * })
+     */
+    private $createdBy;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_date", type="datetime", nullable=true)
+     */
+    private $createdDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modified_date", type="datetime", nullable=true)
+     */
+    private $modifiedDate;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="modified_by", referencedColumnName="id")
+     * })
+     */
+    private $modifiedBy;
+}
---
+++ src/AppBundle/Entity/Organization/StaffHistory.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright (c) 2019. Anime Twin Cities, Inc.
+ *
+ * This project, including all of the files and their contents, is licensed under the terms of MIT License
+ *
+ * See the LICENSE file in the root of this project for details.
+ */
+declare(strict_types=1);
+
+
+namespace AppBundle\Entity\Organization;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use \AppBundle\Entity\User;
+
+/**
+ * Badge
+ *
+ * @ORM\Table(name="organization_staff_history",
+ *     indexes={
+ *      @ORM\Index(columns={"created_by"}),
+ *      @ORM\Index(columns={"modified_by"})
+ *     }
+ * )
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\Organization\StaffHistoryRepository")
+ */
+class StaffHistory
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var Staff
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Organization\Staff")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="staff_id", referencedColumnName="id")
+     * })
+     */
+    private $staff;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="created_by", referencedColumnName="id")
+     * })
+     */
+    private $createdBy;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_date", type="datetime", nullable=true)
+     */
+    private $createdDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modified_date", type="datetime", nullable=true)
+     */
+    private $modifiedDate;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="modified_by", referencedColumnName="id")
+     * })
+     */
+    private $modifiedBy;
+}
