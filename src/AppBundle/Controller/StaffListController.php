d224e3c493481d246ed05aa19d16abcab0214a7f
John J. Koniges <john@koniges.net>
2019-04-09 21:15:30 -0500

    Get datatables working for showing staff list

--- src/AppBundle/Controller/StaffListController.php
+++ src/AppBundle/Controller/StaffListController.php
@@ -20,13 +20,52 @@ use Symfony\Component\Routing\Annotation\Route;
 class StaffListController extends Controller
 {
     /**
-     * @Route("/vue", name="vue")
+     * @Route("/staff", name="staff_list")
      * @Security("has_role('ROLE_USER')")
      *
      * @return Response
      */
-    public function indexAction()
+    public function index()
     {
         return $this->render('organization/staffList.html.twig');
+    }
+
+    /**
+     * @Route("/ajax/staff-list", name="ajax_staff_list")
+     * @Security("has_role('ROLE_USER')")
+     *
+     * @return Response
+     */
+    public function ajaxStaffList()
+    {
+        $returnArray = [
+            "data" => [
+                [
+                    "Tiger Nixon",
+                    "System Architect",
+                    "Edinburgh",
+                    "5421",
+                    "2011/04/25",
+                    "$320,800"
+                ],
+                [
+                    "Garrett Winters",
+                    "Accountant",
+                    "Tokyo",
+                    "8422",
+                    "2011/07/25",
+                    "$170,750"
+                ],
+                [
+                    "Ashton Cox",
+                    "Junior Technical Author",
+                    "San Francisco",
+                    "1562",
+                    "2009/01/12",
+                    "$86,000"
+                ],
+            ]
+        ];
+        return $this->json($returnArray);
     }
 }
